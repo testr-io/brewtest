124d78feee58d5e55d30ceac89770f2bc3e4311b
Shahar Yakir <shahar.yakir@gmail.com>
2020-05-03 14:53:11 +0300

    -

---
+++ up9-darwin-x.rb
@@ -0,0 +1,31 @@
+class Up9DarwinX < Formula
+  desc ""
+  homepage ""
+  url "https://up9cli.s3.amazonaws.com/up9-darwin-x64.tar.gz"
+  sha256 "d5837369038cf3148872e2cbe62779643bdad0134573c5d783f1b3b76815b610"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test up9-darwin-x`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
