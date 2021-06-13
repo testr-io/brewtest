cd2d0d5030a40ca1706b48d6189ef5a9705d91a7
Travis CI User <travis@example.org>
2021-06-13 09:22:35 +0000

    "2.10.0"

--- up9.rb
+++ up9.rb
@@ -2,7 +2,7 @@ class Up9 < Formula
   desc "Everything you need to get started with up9"
   homepage "https://docs.up9.com"
   url "https://up9cli.s3.amazonaws.com/up9-darwin-x64.tar.gz"
-  sha256 "ff811dd99aa4e95ea54fd596df008df065023597b544d75db0b9c1f98ba32412"
+  sha256 "0e9648e34a59b1a741bcb216ca93001bab81de654873252b2086f45f69b1d46f"
 
   def install
     inreplace "bin/up9", /^CLIENT_HOME=/, "export UP9_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
